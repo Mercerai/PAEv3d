d1ae947ee57b62e6be3a6bc47b50747844f7331b
Bright171401 <63548932+Bright171401@users.noreply.github.com>
2024-04-10 19:10:41 +0800

    Add pretrain models

--- event_flow/dataloader/utils.py
+++
@@ -1,9 +0,0 @@
-from progress.bar import Bar
-
-
-class ProgressBar(Bar):
-    suffix = "%(percent).1f%%, ETA: %(eta)ds, %(frequency)fHz"
-
-    @property
-    def frequency(self):
-        return 1 / self.avg
